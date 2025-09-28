2fc23044b9e120e568e89aa555966c1bec3b5a69
Kim Rydén <threezool@gmail.com>
2025-09-28 19:15:24 +0200

    Moved database options in to separate context

---
+++ Jourlity.Data/Context/CaseContext.cs
@@ -0,0 +1,49 @@
+﻿using Jourlity.Data.Entities;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jourlity.Data.Context;
+
+public class CaseContext : DbContext
+{
+    private readonly string _folderPath;
+
+    public DbSet<Entry> Entry { get; set; }
+    
+    public CaseContext(string folderPath)
+    {
+        this._folderPath = folderPath;
+    }
+        
+    public CaseContext(DbContextOptions options) : base(options)
+    {
+        this._folderPath = "";
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder.IsConfigured) 
+            return;
+        
+        var caseFolder = GetBasePathForContext(_folderPath);
+        
+        Directory.CreateDirectory(caseFolder);
+            
+        var connectionString = new SqliteConnectionStringBuilder
+        {
+            DataSource = Path.Combine(caseFolder, "database.sqlite"),
+            Mode = SqliteOpenMode.ReadWriteCreate, 
+        }.ToString();
+
+        optionsBuilder.UseSqlite(connectionString);
+    }
+
+    private static string GetBasePathForContext(string folderPath)
+    {
+        var applicationFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Jourlity");
+
+        applicationFolder = Path.Combine(applicationFolder, folderPath);
+
+        return applicationFolder;
+    }
+}
--- Jourlity.Data/Context/JourlityContext.cs
+++ Jourlity.Data/Context/JourlityContext.cs
@@ -1,4 +1,5 @@
-﻿using Jourlity.Data.Models;
+﻿using Jourlity.Data.Entities;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
 namespace Jourlity.Data.Context
@@ -12,9 +13,50 @@ namespace Jourlity.Data.Context
             
         }
         
-        public JourlityContext(DbContextOptions options) : base(options)
+        public JourlityContext(DbContextOptions options) : base(SetOptions(options))
         {
             
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured) 
+                return;
+        
+            var applicationFolder = GetBasePathForContext();
+            
+            var connectionString = new SqliteConnectionStringBuilder
+            {
+                DataSource = Path.Combine(applicationFolder, "database.sqlite"),
+                Mode = SqliteOpenMode.ReadWriteCreate, 
+            }.ToString();
+
+            optionsBuilder.UseSqlite(connectionString);
+        }
+
+        private static DbContextOptions<JourlityContext> SetOptions(DbContextOptions options)
+        {
+            var appDataDir = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string appSpecificFolder = Path.Combine(appDataDir, "Jourlity");
+        
+            Directory.CreateDirectory(appSpecificFolder);
+        
+            var connectionString = new SqliteConnectionStringBuilder
+            {
+                DataSource = Path.Combine(Path.Combine(appSpecificFolder), "database.sqlite"),
+                Mode = SqliteOpenMode.ReadWriteCreate, 
+            }.ToString();
+            
+            return new DbContextOptionsBuilder<JourlityContext>()
+                .UseSqlite(connectionString)
+                .Options;
+        }
+        
+        private static string GetBasePathForContext()
+        {
+            var applicationFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Jourlity");
+
+            return applicationFolder;
         }
     }
 }
