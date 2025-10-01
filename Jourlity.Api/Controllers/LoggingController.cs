f547b36d6d371fae0ff81f77efd8f1955094d14b
Kim Rydén <threezool@gmail.com>
2025-10-01 18:59:32 +0200

    Added a logging end-point for the app to send to the backend

---
+++ Jourlity.Api/Controllers/LoggingController.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using Jourlity.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("Api/[controller]")]
+public class LoggController : ControllerBase
+{
+    private readonly ILogger<LoggController> _logger;
+
+    public LoggController(ILogger<LoggController> logger)
+    {
+        _logger = logger;
+    }
+    
+    [HttpPost]
+    public HttpMessage<object> Post(HttpMessage<Exception> content)
+    {
+        _logger.LogCritical(content.Object, content.Object?.Message);
+
+        return new HttpMessage<object>
+        {
+            HttpStatusCode = HttpStatusCode.Created,
+            Message = "Success",
+            Object = null
+        };
+    }
+}
---
+++ Jourlity.Dto/HttpMessage.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Jourlity.Dto;
+
+public class HttpMessage<T> where T : class
+{
+    public HttpStatusCode HttpStatusCode { get; set; }
+    public string? Message { get; set; }
+    public T? Object { get; set; }
+}
