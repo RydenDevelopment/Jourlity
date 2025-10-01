e6c717ab1ece4a2bebca9dd7c5d1914f7607ee65
Kim Rydén <threezool@gmail.com>
2025-10-01 19:03:38 +0200

    Updated UI and added icon library

--- Jourlity.App/MauiProgram.cs
+++ Jourlity.App/MauiProgram.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Diagnostics;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using Jourlity.Core.Services;
+using Microsoft.Extensions.Logging;
 using Jourlity.Data.Context;
 using Jourlity.Data.Entities;
 using Jourlity.Data.Repository;
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
+using Jourlity.Dto;
+using Newtonsoft.Json;
 using Entry = Jourlity.Data.Entities.Entry;
 
 namespace Jourlity.App;
@@ -16,22 +20,21 @@ public static class MauiProgram
         builder
             .UseMauiApp<App>()
             .ConfigureFonts(fonts => { fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"); });
-
+        
 #if DEBUG
         builder.Services.AddBlazorWebViewDeveloperTools();
         builder.Logging.AddDebug();
+        AppDomain.CurrentDomain.FirstChanceException += (s, e) =>
+        {
+            System.Diagnostics.Debug.WriteLine("********** OMG! FirstChanceException **********");
+            System.Diagnostics.Debug.WriteLine(e.Exception);
+        };
 #endif
 
         SettingUpDatabase(builder);
         SettingUpServices(builder);
 
         var app = builder.Build();
-
-        AppDomain.CurrentDomain.FirstChanceException += (s, e) =>
-        {
-            System.Diagnostics.Debug.WriteLine("********** OMG! FirstChanceException **********");
-            System.Diagnostics.Debug.WriteLine(e.Exception);
-        };
         
         return app;
     }
@@ -40,8 +43,20 @@ public static class MauiProgram
     {
         builder.Services.AddMauiBlazorWebView();
         
+        builder.Services.AddHttpClient("Backend", client =>
+        {
+            var backendUrl = Environment.GetEnvironmentVariable("BackendUrl");
+            if (backendUrl == null)
+                throw new KeyNotFoundException("No backend url has been configured");
+            
+            client.BaseAddress = new Uri(backendUrl);
+            client.Timeout = TimeSpan.FromSeconds(10);
+        });
+        
         builder.Services.AddScoped<IRepository<Client>, Repository<Client, JourlityContext>>();
         builder.Services.AddScoped<IRepository<Entry>, Repository<Entry, CaseContext>>();
+
+        builder.Services.AddScoped<IBackendCommunicator, BackendCommunicator>();
     }
 
     private static void SettingUpDatabase(MauiAppBuilder builder)
@@ -49,4 +64,5 @@ public static class MauiProgram
         builder.Services.AddDbContext<JourlityContext>();
         builder.Services.AddDbContext<CaseContext>();
     }
+    
 }
