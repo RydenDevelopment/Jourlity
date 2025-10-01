9ad0f7f9254ced4f2c41a0ebd41a79a578864859
Kim Rydén <threezool@gmail.com>
2025-10-01 19:03:00 +0200

    Added service for talking to the backend

---
+++ Jourlity.Core/Services/BackendCommunicator.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using Jourlity.Dto;
+using Newtonsoft.Json;
+
+namespace Jourlity.Core.Services;
+
+public class BackendCommunicator : IBackendCommunicator
+{
+    private readonly HttpClient _client;
+
+
+    public BackendCommunicator(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public async Task LoggErrorToBackend(Exception ex, string message)
+    {
+        var package = new HttpMessage<Exception>
+        {
+            Message = message,
+            Object = ex,
+        };
+        
+        var json = JsonConvert.SerializeObject(package);
+        var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+        var result = await _client.PostAsync("/Logging", data);
+        string resultContent = await result.Content.ReadAsStringAsync();
+    }
+}
---
+++ Jourlity.Core/Services/IBackendCommunicator.cs
@@ -0,0 +1,6 @@
+﻿namespace Jourlity.Core.Services;
+
+public interface IBackendCommunicator
+{
+    public Task LoggErrorToBackend(Exception ex, string message);
+}
